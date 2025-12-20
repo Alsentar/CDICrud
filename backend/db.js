ad813d149a2722ea1b3653c86b0358bd408cecd4
Alsentar <F.alcantara2103@gmail.com>
2025-12-20 00:25:54 -0300

    Sprint 3: Moved Database to Supabase cloud database

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,7 @@
 const pg = require("pg");
+const dns = require("dns");
+
+dns.setDefaultResultOrder("ipv4first");
 
 const Pool = pg.Pool;
 
@@ -6,11 +9,8 @@ require("dotenv").config();
 
 const pool = new Pool({
 
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {rejectUnauthorized: false}
 
 });
 
--- backend/server.js
+++ backend/server.js
@@ -23,4 +23,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
 
     console.log(`Servidor corriendo en puerto ${PORT}`);
+    console.log(process.env.DATABASE_URL);
 })
